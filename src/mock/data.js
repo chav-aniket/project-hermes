7fbfe796daa807e1ded192727f7ef92d495a92dc
Aniket Chavan <z5265106@student.unsw.edu.au>
2021-07-17 02:05:32 +1000

    Simplified placeholder interface

---
+++ src/mock/data.js
@@ -0,0 +1,15 @@
+import { nanoid } from 'nanoid';
+
+// HEAD DATA
+export const headData = {
+  title: 'Aniket Chavan | Full-Stack Dev',
+  lang: 'en',
+  description: 'Welcome to my website!',
+};
+
+// HERO DATA
+export const heroData = {
+  title: 'Hi, my name is ',
+  name: 'Aniket Chavan',
+  subtitle: "I'm the Unknown Developer.",
+};
