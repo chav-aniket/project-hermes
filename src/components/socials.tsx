44d5b3bcce6b98ed3eed2d3e4a2925be06c99c6b
Aniket Chavan <z5265106@student.unsw.edu.au>
2021-07-18 00:52:59 +1000

    Webfonts fixed and icons changed

--- src/components/socials.tsx
+++ src/components/socials.tsx
@@ -1,24 +1,4 @@
-import React, { ReactElement } from 'react';
-// import {
-//   FontAwesomeIcon,
-//   FontAwesomeIconProps,
-// } from '@fortawesome/react-fontawesome'
-// import { IconProp, IconDefinition } from '@fortawesome/fontawesome-svg-core'
-// import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
-// import { faFilePdf } from '@fortawesome/free-solid-svg-icons'
-
-// interface IconProps extends FontAwesomeIconProps {
-//   cls: String,
-//   icon: IconProp,
-//   onClick: React.MouseEventHandler<SVGSVGElement>
-// }
-
-// const Icon: FunctionComponent<IconProps> = (cls: string, icon: IconProp, url: string) => (
-//   <FontAwesomeIcon
-//     className={`icons ${cls}`}
-//     icon={icon}
-//     onClick={() => window.open(url, '_blank')} />
-// );
+import React from 'react';
 
 const Icon = (props: { cls: string; url: string }) => (
   <div className={`icon ${props.cls}`} onClick={() => window.open(props.url, '_blank')}></div>
