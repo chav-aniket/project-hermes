7e64bf028bde4a30c9622b30abd5cf536f80fa71
Aniket Chavan <z5265106@student.unsw.edu.au>
2021-07-18 01:22:16 +1000

    Error fixed'

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, ReactElement } from 'react';
 import { Container } from 'react-bootstrap';
-import { useStaticQuery, graphql } from "gatsby"
+import { useStaticQuery, graphql } from "gatsby";
 import Fade from 'react-reveal/Fade';
 import Socials from './socials';
 
