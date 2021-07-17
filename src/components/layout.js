28f9912a4a8e047abcf85a42a80bb6462056b2fd
Aniket Chavan <z5265106@student.unsw.edu.au>
2021-07-17 20:00:50 +1000

    svg sized and filled

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,3 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
-
 import * as React from "react"
 import PropTypes from "prop-types"
 // import { useStaticQuery, graphql } from "gatsby"
@@ -26,13 +19,7 @@ const Layout = ({ children }) => {
   return (
     <>
       {/* <Header siteTitle={data.site.siteMetadata?.title || `Title`} /> */}
-      <div
-        style={{
-          // margin: `0`,
-          // maxWidth: 960,
-          // padding: `0`,
-        }}
-      >
+      <div>
         <main>{children}</main>
         {/* <footer
           style={{
