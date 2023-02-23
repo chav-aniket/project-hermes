4a84f94c13460f4a3b78a3796057348bc4f1b6af
Aniket Chavan <chavaniket@duck.com>
2023-02-24 01:02:58 +1100

    vite-pwa configured

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,55 @@
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
 import solidJs from "@astrojs/solid-js";
+import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
-
 // https://astro.build/config
-import svelte from "@astrojs/svelte";
+import AstroPWA from "@vite-pwa/astro";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs(), tailwind(), svelte()],
+  integrations: [
+    solidJs(),
+    tailwind(),
+    svelte(),
+    AstroPWA({
+      mode: "development",
+      base: "/",
+      scope: "/",
+      includeAssets: ["favicon.svg"],
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Hermes",
+        short_name: "Hermes",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ["**/*.{css,js,html,svg,png,ico,txt}"],
+      },
+      devOptions: {
+        enabled: true,
+        navigateFallback: "/404",
+      },
+    }),
+  ],
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
 });
