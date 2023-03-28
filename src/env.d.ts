31b22657bf6514669f7b32df512e7179588a3567
Aniket Chavan <chavaniket@duck.com>
2023-03-28 23:59:30 +1100

    added github api fetching for repos

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,12 @@
 /// <reference types="astro/client" />
 /// <reference types="vite-plugin-pwa/client" />
 /// <reference types="vite-plugin-pwa/info" />
+
+interface ImportMetaEnv {
+    readonly GH_TOKEN: string;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
+
---
+++ src/utils/github.ts
@@ -0,0 +1,35 @@
+import { Octokit } from "@octokit/core";
+import { restEndpointMethods } from "@octokit/plugin-rest-endpoint-methods";
+
+export type Repo = {
+    name: string,
+    description: string,
+    language: string,
+    stargazers: number,
+    link: string,
+};
+
+const MyOcto = Octokit.plugin(restEndpointMethods);
+const octo = new MyOcto({
+    auth: import.meta.env.GH_TOKEN,
+});
+
+export const fetchRepos = async (): Promise<Repo[]> => {
+    const resp = await octo.rest.repos.listForAuthenticatedUser({
+        visibility: "public",
+        affiliation: "owner",
+    });
+
+    const pinned = resp.data.filter(r => r.topics?.includes('pinned'));
+    
+    const repos = pinned.map(p => ({
+        name: p.name,
+        description: p.description ?? "",
+        language: p.language ?? "",
+        stargazers: p.stargazers_count,
+        link: p.html_url
+    }));
+    
+    return repos;
+}
+
