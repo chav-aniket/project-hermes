fe6d21b10694f8640c8c474a98d446a4e2920098
Aniket Chavan <chavaniket@duck.com>
2023-04-16 19:37:58 +1000

    new favicons

--- astro.config.mjs
+++ astro.config.mjs
@@ -24,17 +24,17 @@ export default defineConfig({
         theme_color: "#ffffff",
         icons: [
           {
-            src: "pwa-192x192.png",
+            src: "hermes-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "pwa-512x512.png",
+            src: "hermes-512x512.png",
             sizes: "512x512",
             type: "image/png",
           },
           {
-            src: "pwa-512x512.png",
+            src: "hermes-512x512.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "any maskable",
